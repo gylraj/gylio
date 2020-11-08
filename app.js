0c5879f56a0d4362f74ab3b0def870afdff022ad
gylraj <gianolivar@gmail.com>
2020-11-09 02:36:59 +0800

    check error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
+// const socketIo = require("socket.io");
 
 const port = process.env.PORT || 443;
 const index = require("./routes/index");
@@ -10,26 +10,26 @@ app.use(index);
 
 const server = http.createServer(app);
 
-const io = socketIo(server);
+// const io = socketIo(server);
 
-let interval;
+// let interval;
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
-});
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
 
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
+// const getApiAndEmit = socket => {
+//   const response = new Date();
+//   // Emitting a new message. Will be consumed by the client
+//   socket.emit("FromAPI", response);
+// };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
